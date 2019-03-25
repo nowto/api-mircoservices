50624865cc0b968d44906a8fe7af3471fef8db67
weibo.li <weibo.li@mydeertrip.com>
2019-03-25 10:42:46 +0800

    添加author

--- xunlu-cloud-user/src/main/java/com/xunlu/api/user/UserMicroserviceApplication.java
+++ xunlu-cloud-user/src/main/java/com/xunlu/api/user/UserMicroserviceApplication.java
@@ -3,6 +3,9 @@ package com.xunlu.api.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author liweibo
+ */
 @SpringBootApplication
 public class UserMicroserviceApplication {
 
