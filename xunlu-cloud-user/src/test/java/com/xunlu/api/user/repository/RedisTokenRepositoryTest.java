c2337addd2612afcae8b895a7d689f58bbab869d
weibo.li <weibo.li@mydeertrip.com>
2019-04-09 16:56:50 +0800

    RedisTokenRepository单元测试

---
+++ xunlu-cloud-user/src/test/java/com/xunlu/api/user/repository/RedisTokenRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.xunlu.api.user.repository;
+
+import com.xunlu.api.user.domain.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import static org.mockito.Mockito.*;
+
+public class RedisTokenRepositoryTest {
+
+    @Test
+    public void findOne() {
+        ValueOperations<String, Object> opsForValue = mock(ValueOperations.class);
+        when(opsForValue.get("token:userId:4")).thenReturn("hello");
+
+        RedisTemplate<String, Object> redisTemplate = mock(RedisTemplate.class);
+        when(redisTemplate.opsForValue()).thenReturn(opsForValue);
+
+        RedisTokenRepository tokenRepository = new RedisTokenRepository(redisTemplate);
+        User user = new User();
+        user.setId(4);
+
+        String token = tokenRepository.findOne(user);
+
+        Assert.assertEquals("hello", token);
+    }
+
+    @Test
+    public void addToken() {
+        ValueOperations<String, Object> opsForValue = mock(ValueOperations.class);
+
+        RedisTemplate<String, Object> redisTemplate = mock(RedisTemplate.class);
+        when(redisTemplate.opsForValue()).thenReturn(opsForValue);
+
+        RedisTokenRepository tokenRepository = new RedisTokenRepository(redisTemplate);
+
+        User u = new User();
+        u.setId(4);
+        tokenRepository.addToken(u, "hello");
+
+        verify(opsForValue).set("token:userId:4", "hello");
+        verify(opsForValue).set("user:token:hello", u);
+    }
+
+    @Test
+    public void deleteToken() {
+        ValueOperations<String, Object> opsForValue = mock(ValueOperations.class);
+        when(opsForValue.get("token:userId:4")).thenReturn("hello");
+
+        RedisTemplate<String, Object> redisTemplate = mock(RedisTemplate.class);
+        when(redisTemplate.opsForValue()).thenReturn(opsForValue);
+
+        RedisTokenRepository tokenRepository = new RedisTokenRepository(redisTemplate);
+
+        tokenRepository.deleteToken(4);
+
+        verify(redisTemplate).delete("user:token:hello");
+        verify(redisTemplate).delete("token:userId:4");
+    }
+}
