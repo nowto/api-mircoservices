c7bc698852c996a1ea55af8621dc5fd97421428a
weibo.li <weibo.li@mydeertrip.com>
2019-04-11 19:45:01 +0800

    gateway 重命名， 集成配置中心

--- api-gateway/src/main/java/com/xunlu/api/gateway/GatewayApplication.java
+++ api-gateway/src/main/java/com/xunlu/api/gateway/GatewayApplication.java
@@ -2,10 +2,14 @@ package com.xunlu.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
+@EnableCircuitBreaker
 public class GatewayApplication {
 
     public static void main(String[] args) {
