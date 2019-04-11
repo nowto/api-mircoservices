9ba63799630b6b74a91c35e5986fef8b24460dfb
weibo.li <weibo.li@mydeertrip.com>
2019-04-11 19:04:01 +0800

    完成配置中心

---
+++ config-server/src/main/java/com/xunlu/api/config/server/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.xunlu.api.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author liweibo
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
