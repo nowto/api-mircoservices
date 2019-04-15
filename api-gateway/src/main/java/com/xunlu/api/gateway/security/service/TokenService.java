f2ff86810ab34cdb0892eff560db9749d71c2623
weibo.li <weibo.li@mydeertrip.com>
2019-04-15 19:23:27 +0800

    完成tokenService

---
+++ api-gateway/src/main/java/com/xunlu/api/gateway/security/service/TokenService.java
@@ -0,0 +1,17 @@
+package com.xunlu.api.gateway.security.service;
+
+import com.xunlu.api.gateway.security.domain.User;
+
+/**
+ * token服务类
+ * @author liweibo
+ */
+public interface TokenService {
+
+    /**
+     * 根据token获取用户信息
+     * @param token token
+     * @return 用户， 如果获取不到将返回null
+     */
+    User getUser(String token);
+}
---
+++ api-gateway/src/main/java/com/xunlu/api/gateway/security/service/TokenServiceImpl.java
@@ -0,0 +1,26 @@
+package com.xunlu.api.gateway.security.service;
+
+import com.xunlu.api.gateway.security.domain.User;
+import com.xunlu.api.gateway.security.repository.TokenRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * TokenService实现
+ * @author liweibo
+ */
+@Service
+public class TokenServiceImpl implements TokenService {
+    private TokenRepository tokenRepository;
+
+    public TokenServiceImpl(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+
+    @Override
+    public User getUser(String token) {
+        if (token == null) {
+            return null;
+        }
+        return tokenRepository.getUserByToken(token);
+    }
+}
