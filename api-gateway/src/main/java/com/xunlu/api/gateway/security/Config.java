d467179d2fc961a281b00c862f5759a69ee995c7
weibo.li <weibo.li@mydeertrip.com>
2019-04-18 14:59:58 +0800

    完成退出登录功能

--- api-gateway/src/main/java/com/xunlu/api/gateway/security/Config.java
+++ api-gateway/src/main/java/com/xunlu/api/gateway/security/Config.java
@@ -12,8 +12,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.ForwardAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * 配置Spring Security.
@@ -23,6 +31,7 @@ import org.springframework.security.web.authentication.logout.LogoutFilter;
 @EnableWebSecurity
 public class Config extends WebSecurityConfigurerAdapter {
     public static final String AUTHENTICATION_FAILURE_FORWARD_URL = "/authentication_failure";
+
     @Autowired
     private TokenService tokenService;
 
@@ -54,88 +63,92 @@ public class Config extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .anonymous().and()
-                .formLogin().disable()
-                .csrf().disable()
-                .cors().disable()
-                .logout().disable()
-                .httpBasic().disable()
-                .authorizeRequests()// 网关负责鉴权
-                    .antMatchers(
-                            "/act/**",
-                            "/article/share",
-                            "/article/collect/option",
-                            "/article/collect/query",
-                            "/article/collect/status",
-                            "/chart/plans",
-                            "/chart/plan/del",
-                            "/feature/**",
-                            "/user/feedback/**",
-                            "/hotel/**",
-                            "/logout",
-                            "/merchant/addMerchantEntry",
-                            "/plan/delPlan",
-                            "/plan/renamePlan",
-                            "/plan/timeMeasure",
-                            "/plan/geneby",
-                            "/plan/smallTraffic/list",
-                            "/plan/smallTraffic",
-                            "/plan/train/list",
-                            "/plan/hotel/list",
-                            "/plan/hotel/list/filter",
-                            "/plan/hotel",
-                            "/plan/collect/add",
-                            "/plan/collect/cancel",
-                            "/plan/share",
-                            "/plan/listFlight",
-                            "/plan/changeFlight",
-                            "/plan/changeTrain",
-                            "/plan/listHotel",
-                            "/plan/changeHotel",
-                            "/plan/save",
-                            "/plan/be56e057f20f883e",
-                            "/plan/adjust/sight",
-                            "/plan/quote",
-                            "/plan/listRoute/filter",
-                            "/plan/listssNew",
-                            "/plan/listss/filter",
-                            "/plan/listss",
-                            "/plan/ssList/filter",
-                            "/plan/routeList",
-                            "/plan/routeList/filter",
-                            "/plan/listssByAdd",
-                            "/route/collect/add",
-                            "/route/collect/cancel",
-                            "/route/share",
-                            "/scenic_spots/editPlanSS",
-                            "/scenic_spots/collect/option",
-                            "/scenic_spots/share",
-                            "/system/*",
-                            "/topic/collect/add",
-                            "/topic/collect/cancel",
-                            "/topic/share",
-                            "/user/plan/list",
-                            "/user/collect/ss",
-                            "/user/collect/plan",
-                            "/user/collect/topic",
-                            "/user/collect/travels",
-                            "/user/collect/route",
-                            "/user/userCenter",
-                            "/user/prefer",
-                            "/user/nickname/modify",
-                            "/user/personsign/modify",
-                            "/user/password/modify",
-                            "/user/photo/uptoken"
-                    )
-                        .hasRole("USER")
-                    .anyRequest().permitAll();//剩余的都是不需要登录也能访问的请求
+                .and().formLogin().disable().httpBasic().disable().csrf().disable().cors().disable();
 
+        // 鉴权
+        http.authorizeRequests()// 网关负责鉴权
+            .antMatchers(
+                    "/logout",
+                    "/act/**",
+                    "/article/share",
+                    "/article/collect/option",
+                    "/article/collect/query",
+                    "/article/collect/status",
+                    "/chart/plans",
+                    "/chart/plan/del",
+                    "/feature/**",
+                    "/user/feedback/**",
+                    "/hotel/**",
+                    "/logout",
+                    "/merchant/addMerchantEntry",
+                    "/plan/delPlan",
+                    "/plan/renamePlan",
+                    "/plan/timeMeasure",
+                    "/plan/geneby",
+                    "/plan/smallTraffic/list",
+                    "/plan/smallTraffic",
+                    "/plan/train/list",
+                    "/plan/hotel/list",
+                    "/plan/hotel/list/filter",
+                    "/plan/hotel",
+                    "/plan/collect/add",
+                    "/plan/collect/cancel",
+                    "/plan/share",
+                    "/plan/listFlight",
+                    "/plan/changeFlight",
+                    "/plan/changeTrain",
+                    "/plan/listHotel",
+                    "/plan/changeHotel",
+                    "/plan/save",
+                    "/plan/be56e057f20f883e",
+                    "/plan/adjust/sight",
+                    "/plan/quote",
+                    "/plan/listRoute/filter",
+                    "/plan/listssNew",
+                    "/plan/listss/filter",
+                    "/plan/listss",
+                    "/plan/ssList/filter",
+                    "/plan/routeList",
+                    "/plan/routeList/filter",
+                    "/plan/listssByAdd",
+                    "/route/collect/add",
+                    "/route/collect/cancel",
+                    "/route/share",
+                    "/scenic_spots/editPlanSS",
+                    "/scenic_spots/collect/option",
+                    "/scenic_spots/share",
+                    "/system/*",
+                    "/topic/collect/add",
+                    "/topic/collect/cancel",
+                    "/topic/share",
+                    "/user/plan/list",
+                    "/user/collect/ss",
+                    "/user/collect/plan",
+                    "/user/collect/topic",
+                    "/user/collect/travels",
+                    "/user/collect/route",
+                    "/user/userCenter",
+                    "/user/prefer",
+                    "/user/nickname/modify",
+                    "/user/personsign/modify",
+                    "/user/password/modify",
+                    "/user/photo/uptoken"
+            ).hasRole("USER")
+            .anyRequest().permitAll();//剩余的都是不需要登录也能访问的请求
+
+        // token 认证功能
         TokenAuthenticationFilter tokenAuthenticationFilter = new TokenAuthenticationFilter("/user-service/token");
         tokenAuthenticationFilter.setAuthenticationManager(authenticationManagerBean());
         tokenAuthenticationFilter.setAuthenticationFailureHandler(new ForwardAuthenticationFailureHandler(AUTHENTICATION_FAILURE_FORWARD_URL));
         http.addFilterAfter(tokenAuthenticationFilter, LogoutFilter.class);
 
+        // 退出登录功能
+        http.logout()
+                .addLogoutHandler(new TokenLogoutHandler(tokenService))
+                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+
+        // 开启游客模式，允许一些api不用登录也能被访问
+        http.anonymous();
     }
 
     @Override
