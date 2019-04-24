d4b31cd1f0253806d5ae8b35228fd70989d11406
weibo.li <weibo.li@mydeertrip.com>
2019-04-24 23:24:20 +0800

    Page重命名

--- api-common-spring-boot-starter/src/main/java/com/xunlu/api/common/restful/condition/Page.java
+++ api-common-spring-boot-starter/src/main/java/com/xunlu/api/common/restful/condition/Page.java
@@ -10,7 +10,7 @@ import java.util.List;
  * {@link ResponseEntity}实现, 便于返回分页的资源集合数据.
  * @param <T> 资源类
  */
-public class PageResponseEntity<T> extends ResponseEntity<List<T>> {
+public class Page<T> extends ResponseEntity<List<T>> {
     public static final String TOTAL_COUNT_RESPONSE_HEADER_NAME = "X-TOTAL-COUNT";
 
     /**
@@ -18,7 +18,7 @@ public class PageResponseEntity<T> extends ResponseEntity<List<T>> {
      * @param pageData 代表某查询条件下一页的资源,将作为{@link ResponseEntity#body}输出
      * @param totalCount 代表某查询条件下该资源的总数目, 将使用自定义响应头{@link #TOTAL_COUNT_RESPONSE_HEADER_NAME X-TOTAL-COUNT}输出
      */
-    public PageResponseEntity(List<T> pageData, int totalCount) {
+    public Page(List<T> pageData, int totalCount) {
         super(pageData, totalCountHeader(totalCount), HttpStatus.OK);
 
     }
@@ -30,11 +30,11 @@ public class PageResponseEntity<T> extends ResponseEntity<List<T>> {
     }
 
     /**
-     * 构造一个集合的响应实体.
+     * 构造一个集合的响应实体.(单页返回全部)
      * 主要用于不需要分页, 全部返回的情况, 此时不会输出 {@link #TOTAL_COUNT_RESPONSE_HEADER_NAME X-TOTAL-COUNT}
      * @param pageData
      */
-    public PageResponseEntity(List<T> pageData) {
+    public Page(List<T> pageData) {
         super(pageData, HttpStatus.OK);
     }
 }
